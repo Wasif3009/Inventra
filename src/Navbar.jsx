0438e9733b93c31c5959ecc8d4184ca96407ecee
Wasif3009 <wasifshaikh1233@gmail.com>
2026-05-05 17:29:46 +0530

    commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = ({}) => {
   };
 
   return (
-    <div className="section-1  min-h-screen flex flex-col ">
+    <div className="section-1  flex flex-col ">
       <div className="image  flex flex-col items-center w-3xs justify-center md:w-sm ">
         <img src="InventraSided.png" alt="Inventra Logo" className="w-3xs" />
       </div>
